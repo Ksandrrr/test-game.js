fc9673a987abe3aff925c3eda37782350d6b2651
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-01-09 21:41:49 +0200

    #2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,7 @@ function getRandomHexColor() {
 function getRandomNumb() {
 return `${Math.floor(Math.random() * (90 - 1) + 1)}%`;
 }
-console.log(getRandomNumb())
+
 
 
 const alldiv = document.querySelector(`.game`)
@@ -12,11 +12,15 @@ const divball = document.querySelector(`.div-ball`)
 const start = document.querySelector(`.start`)
 const ballx2 = document.querySelector(`.div-ballx2`)
 const addClass = document.querySelector(`.button-ball`)
-const p = document.querySelector(`p`)
+const p = document.querySelector(`.numb`)
+
+let number = 0;
+console.log(p)
+let random = getRandomNumb();
 
 
 
-
+p.style.opacity = 0;
 
 start.addEventListener(`click`, startGame)
 function startGame() {
@@ -24,19 +28,33 @@ function startGame() {
     divball.style.top = getRandomNumb();
     divball.style.left = getRandomNumb();
     ballx2.style.backgroundColor = getRandomHexColor();
-    start.style.opacity =  0;
+    start.style.opacity = 0;
+    p.style.opacity = 1;
+    p.textContent = number
+    number += 1;
+   
 }
 
 divball.addEventListener(`click`, chancheBall)
 function chancheBall() {
+    p.textContent = number
     divball.classList.toggle(`button-ball`)
     ballx2.style.top = getRandomNumb();
     ballx2.style.left = getRandomNumb();
     ballx2.style.backgroundColor = getRandomHexColor();
+    number += 1;
+    
     if (!divball.classList.contains(`button-ball`)) {
-        ballx2.classList.add(`button-ball`)
-        
+        ballx2.classList.add(`button-ball`)  
     }
+    if (number >= 30) {
+        alert(`Хватить з тебе`)
+        start.style.opacity = 1;
+        divball.classList.remove(`button-ball`)
+        ballx2.classList.remove(`button-ball`)
+        number = 0
+    }
+    
 }
 
 ballx2.addEventListener(`click`, removeGame)
@@ -47,8 +65,6 @@ function removeGame() {
     divball.style.top = getRandomNumb();
     divball.style.left = getRandomNumb();
     divball.style.backgroundColor = getRandomHexColor();
+    p.textContent = number
+    number += 1;
 }
-// start.addEventListener(`click`, StartGame)
-// function StartGame() {
-    
-// }
